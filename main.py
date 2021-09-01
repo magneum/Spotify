fc40784b7da811fe65564bbf5d3747c4107b7fee
HypeVoidSoul <hypevoidsoul@yahoo.com>
2021-09-01 11:27:11 +0100

    VOID_DONE

--- main.py
+++ main.py
@@ -1,35 +1,32 @@
 import os
 import logging
-os.system("clear")
 from telegram.ext import Updater, MessageHandler, Filters
 logging.basicConfig(
-format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 level=logging.INFO)
 runner = Updater("1940504180:AAGg7L53zq9oZgxqeHrfkc4x0nHcrTAzG_0")
 dispatcher = runner.dispatcher
-def get_single_song(bot, update):
-    chat_id = update.effective_message.chat_id
+def VOID_FEEDER(client, update):
+    VOID_ID = update.effective_message.chat_id
     message_id = update.effective_message.message_id
-    url = "'" + update.effective_message.text + "'"
-    os.system(f'mkdir -p .temp{message_id}{chat_id}')
-    os.chdir(f'./.temp{message_id}{chat_id}')
-    logging.log(logging.INFO, f'start downloading')
-    os.system(f'spotdl {url}')
-    logging.log(logging.INFO, 'sending to client') 
-    sent = 0 
-    bot.send_message(chat_id=chat_id, text="Sending to You...")
-    files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(".") for f in filenames if os.path.splitext(f)[1] == '.mp3']
+    url = """ + update.effective_message.text + """
+    os.system(f"mkdir -p .hypespoted{message_id}{VOID_ID}")
+    os.chdir(f"./.hypespoted{message_id}{VOID_ID}")
+    os.system(f"spotdl {url}")
+    DONE = 0 
+    client.send_message(chat_id=VOID_ID,text="Sending to You...")
+    files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(".") for f in filenames if os.path.splitext(f)[1] == ".mp3"]
     for file in files:
-        bot.send_audio(chat_id=chat_id, audio=open(f'./{file}', 'rb'), timeout=1000)
-        sent += 1
-    os.chdir('./..')
-    os.system(f'rm -rf .temp{message_id}{chat_id}')
-    if sent == 0:
-       bot.send_message(chat_id=chat_id,
-       text="It seems there was a problem in finding/sending the song.")
+        client.send_audio(chat_id=VOID_ID, audio=open(f"./{file}", "rb"), timeout=500)
+        DONE += 1
+    os.chdir("./..")
+    os.system(f"rm -rf .hypespoted{message_id}{VOID_ID}")
+    if DONE == 0:
+       client.send_message(chat_id=VOID_ID,
+       text="Error! Check link or report @HypeVoids")
     else:
         pass
-handler = MessageHandler(Filters.text, get_single_song)
-dispatcher.add_handler(handler=handler)
+VOID_DONE = MessageHandler(Filters.text,VOID_FEEDER)
+dispatcher.add_handler(handler=VOID_DONE)
 runner.start_polling(poll_interval=0.8)
 runner.idle()

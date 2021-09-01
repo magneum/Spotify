3aa4164b7c8a11ccaa1949448a156e26424da9ed
HypeVoidSoul <hypevoidsoul@yahoo.com>
2021-09-01 12:10:25 +0100

    S = client.send_message(chat_id=VOID_ID,text="Sending")

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ dispatcher = runner.dispatcher
 def VOID_FEEDER(client, update):
     VOID_ID = update.effective_message.chat_id
     message_id = update.effective_message.message_id
+    S = client.send_message(chat_id=VOID_ID,text="Sending")
     url = """ + update.effective_message.text + """
     os.system(f"mkdir -p .hypespoted{message_id}{VOID_ID}")
     os.chdir(f"./.hypespoted{message_id}{VOID_ID}")
     os.system(f"spotdl {url}")
     DONE = 0 
-    S = client.send_message(chat_id=VOID_ID,text="Sending")
-    files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(".") for f in filenames if os.path.splitext(f)[1] == ".mp3"]
-    for file in files:
+    SEEDED = [os.path.join(dp, f) for dp, dn, filenames in os.walk(".") for f in filenames if os.path.splitext(f)[1] == ".mp3"]
+    for file in SEEDED:
         client.send_audio(chat_id=VOID_ID, audio=open(f"./{file}", "rb"), timeout=500)
         DONE += 1
     os.chdir("./..")

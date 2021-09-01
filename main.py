eae7d04f6649ec9b8956bf2b303b3d78129bffcf
HypeVoidSoul <hypevoidsoul@yahoo.com>
2021-09-01 07:55:19 +0100

    d

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-# import os
-# import time
-# import logging
-# os.system("pip install python-telegram-bot==12.3.0")
-# os.system("pip install spotdl==3.7.2")
-# os.system("clear")
+import os
+import logging
+os.system("pip install python-telegram-bot==12.3.0")
+os.system("pip install spotdl==3.7.2")
+os.system("clear")
 import telegram.ext as tg
-# from telegram.ext import MessageHandler, Filters
+from telegram.ext import MessageHandler, Filters
 
 
 
-# logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
 
 
 
@@ -22,82 +20,6 @@ dispatcher = updater.dispatcher
 
 
 
-
-# def get_single_song(bot, update):
-#     chat_id = update.effective_message.chat_id
-#     message_id = update.effective_message.message_id
-
-#     url = "'" + update.effective_message.text + "'"
-
-#     os.system(f'mkdir -p .temp{message_id}{chat_id}')
-#     os.chdir(f'./.temp{message_id}{chat_id}')
-
-#     FET = bot.send_message(
-#     chat_id=chat_id,
-#     text="Fetching...")
-#     os.system(f'spotdl {url}')
-
-
-#     time.sleep(1)
-#     FET.delete()
-    
-
-#     try:
-#         sent = 0 
-#         SE = bot.send_message(
-#         chat_id=chat_id,
-#         text="Sending to You...")
-
-
-
-
-#         files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(".") for f in filenames if os.path.splitext(f)[1] == '.mp3']
-#         for file in files:
-#             bot.send_audio(chat_id=chat_id, audio=open(f'./{file}', 'rb'), timeout=1000)
-#             sent += 1
-        
-
-
-#         SE.delete()
-#     except:
-#         pass
-
-
-#     os.chdir('./..')
-#     os.system(f'rm -rf .temp{message_id}{chat_id}')
-
-
-
-#     if sent == 0:
-#         bot.send_message(
-#         chat_id=chat_id,
-#         text="It seems there was a problem in finding/sending the song.")
-#     else:
-#         logging.info("Sent Audio")
-
-            
-
-        
-
-# handler = MessageHandler(
-# Filters.text,
-# get_single_song)
-# dispatcher.add_handler(
-# handler=handler)
-# updater.start_polling()
-# updater.idle()
-
-
-
-
-import json
-import logging
-import os
-os.system("pip install python-telegram-bot==12.3.0")
-os.system("pip install spotdl==3.7.2")
-os.system("clear")
-from telegram.ext import Updater, MessageHandler, Filters, Handler
-from telegram import Bot
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
@@ -118,18 +40,31 @@ def get_single_song(bot, update):
     os.chdir(f'./.temp{message_id}{chat_id}')
 
     logging.log(logging.INFO, f'start downloading')
-    bot.send_message(chat_id=chat_id, text="Fetching...")
+    FET = bot.send_message(chat_id=chat_id, text="Fetching...")
     os.system(f'spotdl {url}')
     logging.log(logging.INFO, 'sending to client')
 
     
     try:
         sent = 0 
-        bot.send_message(chat_id=chat_id, text="Sending to You...")
+        SEN = bot.send_message(
+        chat_id=chat_id,
+        text="Sending to You...")
+
+
+        FET.delete()
+
+
+        
         files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(".") for f in filenames if os.path.splitext(f)[1] == '.mp3']
         for file in files:
             bot.send_audio(chat_id=chat_id, audio=open(f'./{file}', 'rb'), timeout=1000)
             sent += 1
+
+            
+
+        SEN.delete()
+
     except:
         pass
 
@@ -137,7 +72,9 @@ def get_single_song(bot, update):
     os.system(f'rm -rf .temp{message_id}{chat_id}')
 
     if sent == 0:
-       bot.send_message(chat_id=chat_id, text="It seems there was a problem in finding/sending the song.")
+       bot.send_message(
+        chat_id=chat_id,
+        text="It seems there was a problem in finding/sending the song.")
     else:
         logging.log(logging.INFO, 'sent')
 
@@ -146,6 +83,7 @@ def get_single_song(bot, update):
 handler = MessageHandler(Filters.text, get_single_song)
 dispatcher.add_handler(handler=handler)
 
-POLLING_INTERVAL = 0.8
-updater.start_polling(poll_interval=POLLING_INTERVAL)
+
+
+updater.start_polling()
 updater.idle()

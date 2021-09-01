443562a3c3f9847917771c857e48b5c334e8fbc3
HypeVoidSoul <hypevoidsoul@yahoo.com>
2021-09-01 07:48:31 +0100

    f

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-import os
-import time
-import logging
-os.system("pip install python-telegram-bot==12.3.0")
-os.system("pip install spotdl==3.7.2")
-os.system("clear")
+# import os
+# import time
+# import logging
+# os.system("pip install python-telegram-bot==12.3.0")
+# os.system("pip install spotdl==3.7.2")
+# os.system("clear")
 import telegram.ext as tg
-from telegram.ext import MessageHandler, Filters
+# from telegram.ext import MessageHandler, Filters
 
 
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+# logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
 
 
 
@@ -23,66 +23,129 @@ dispatcher = updater.dispatcher
 
 
 
+# def get_single_song(bot, update):
+#     chat_id = update.effective_message.chat_id
+#     message_id = update.effective_message.message_id
+
+#     url = "'" + update.effective_message.text + "'"
+
+#     os.system(f'mkdir -p .temp{message_id}{chat_id}')
+#     os.chdir(f'./.temp{message_id}{chat_id}')
+
+#     FET = bot.send_message(
+#     chat_id=chat_id,
+#     text="Fetching...")
+#     os.system(f'spotdl {url}')
+
+
+#     time.sleep(1)
+#     FET.delete()
+    
+
+#     try:
+#         sent = 0 
+#         SE = bot.send_message(
+#         chat_id=chat_id,
+#         text="Sending to You...")
+
+
+
+
+#         files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(".") for f in filenames if os.path.splitext(f)[1] == '.mp3']
+#         for file in files:
+#             bot.send_audio(chat_id=chat_id, audio=open(f'./{file}', 'rb'), timeout=1000)
+#             sent += 1
+        
+
+
+#         SE.delete()
+#     except:
+#         pass
+
+
+#     os.chdir('./..')
+#     os.system(f'rm -rf .temp{message_id}{chat_id}')
+
+
+
+#     if sent == 0:
+#         bot.send_message(
+#         chat_id=chat_id,
+#         text="It seems there was a problem in finding/sending the song.")
+#     else:
+#         logging.info("Sent Audio")
+
+            
+
+        
+
+# handler = MessageHandler(
+# Filters.text,
+# get_single_song)
+# dispatcher.add_handler(
+# handler=handler)
+# updater.start_polling()
+# updater.idle()
+
+
+
+
+import json
+import logging
+import os
+os.system("pip install python-telegram-bot==12.3.0")
+os.system("pip install spotdl==3.7.2")
+os.system("clear")
+from telegram.ext import Updater, MessageHandler, Filters, Handler
+from telegram import Bot
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+
+
+
+
 def get_single_song(bot, update):
     chat_id = update.effective_message.chat_id
     message_id = update.effective_message.message_id
+    username = update.message.chat.username
+    logging.log(logging.INFO, f'start to query message {message_id} in chat:{chat_id} from {username}')
 
     url = "'" + update.effective_message.text + "'"
 
     os.system(f'mkdir -p .temp{message_id}{chat_id}')
     os.chdir(f'./.temp{message_id}{chat_id}')
 
-    FET = bot.send_message(
-    chat_id=chat_id,
-    text="Fetching...")
+    logging.log(logging.INFO, f'start downloading')
+    bot.send_message(chat_id=chat_id, text="Fetching...")
     os.system(f'spotdl {url}')
+    logging.log(logging.INFO, 'sending to client')
 
-
-    time.sleep(1)
-    FET.delete()
     
-
     try:
         sent = 0 
-        SE = bot.send_message(
-        chat_id=chat_id,
-        text="Sending to You...")
-
-
-
-
+        bot.send_message(chat_id=chat_id, text="Sending to You...")
         files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(".") for f in filenames if os.path.splitext(f)[1] == '.mp3']
         for file in files:
             bot.send_audio(chat_id=chat_id, audio=open(f'./{file}', 'rb'), timeout=1000)
             sent += 1
-        
-
-
-        SE.delete()
     except:
         pass
-
 
     os.chdir('./..')
     os.system(f'rm -rf .temp{message_id}{chat_id}')
 
-
-
     if sent == 0:
-        bot.send_message(
-        chat_id=chat_id,
-        text="It seems there was a problem in finding/sending the song.")
+       bot.send_message(chat_id=chat_id, text="It seems there was a problem in finding/sending the song.")
     else:
-        logging.info("Sent Audio")
+        logging.log(logging.INFO, 'sent')
 
-            
 
-        
 
-handler = MessageHandler(
-Filters.text,
-get_single_song)
-dispatcher.add_handler(
-handler=handler)
-updater.start_polling()
+handler = MessageHandler(Filters.text, get_single_song)
+dispatcher.add_handler(handler=handler)
+
+POLLING_INTERVAL = 0.8
+updater.start_polling(poll_interval=POLLING_INTERVAL)
 updater.idle()

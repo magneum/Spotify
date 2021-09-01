4a9859ea155782fdedad07547dfc576970e256a7
HypeVoidSoul <hypevoidsoul@yahoo.com>
2021-09-01 11:29:57 +0100

    S

--- main.py
+++ main.py
@@ -14,13 +14,14 @@ def VOID_FEEDER(client, update):
     os.chdir(f"./.hypespoted{message_id}{VOID_ID}")
     os.system(f"spotdl {url}")
     DONE = 0 
-    client.send_message(chat_id=VOID_ID,text="Sending to You...")
+    S = client.send_message(chat_id=VOID_ID,text="Sending")
     files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(".") for f in filenames if os.path.splitext(f)[1] == ".mp3"]
     for file in files:
         client.send_audio(chat_id=VOID_ID, audio=open(f"./{file}", "rb"), timeout=500)
         DONE += 1
     os.chdir("./..")
     os.system(f"rm -rf .hypespoted{message_id}{VOID_ID}")
+    S.delete()
     if DONE == 0:
        client.send_message(chat_id=VOID_ID,
        text="Error! Check link or report @HypeVoids")
